88b0479434e75d8f15917436198fbd7bdad359f8
R B <roben777@gmail.com>
2015-03-03 02:49:57 -0700

    1. Strategy: greeter.cc, one version in English only

---
+++ greeter.cc
@@ -0,0 +1,37 @@
+// Simple starting example to illustrate the "strategy design pattern"
+// ** Variability **
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+// greeter class: configured with a list of names, outputs a greeting for each name
+class Greeter {
+private:
+	std::vector<std::string> names; // the list of names that the greeter must greet
+
+public:
+	// adds a name to the list of names that this Greeter object must greet
+	void addName(std::string name) {
+		names.push_back(name);
+	}
+
+	// greeting everyone:
+	void greet() {
+		for (std::vector<std::string>::iterator it=names.begin(); it != names.end(); it++) {
+			std::cout << "Hello " << *it << "!" << std::endl;
+		}
+	}
+};
+
+
+// example of using the class
+
+int main() {
+	Greeter john;
+	john.addName("Mark");
+	john.addName("Sophie");
+	john.addName("Lilly");
+	john.greet();
+}
+	
